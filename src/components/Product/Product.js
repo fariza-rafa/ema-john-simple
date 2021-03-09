da00865763b5ae10acd857477a209ab87b5b54e5
Fariza Rafa <farizarafa123@gmail.com>
2021-03-09 22:04:59 +0600

    jibon ta bedona

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import './Product.css';
 const Product = (props) => {
-    console.log(props.product.name);
+    console.log(props.product);
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div>
-            <h3>This is product</h3>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+            </div>
+            <br />
+            <p><small>by: {seller}</small></p>
+            <p>${price}</p>
+            <p><small>Only {stock} left in stock - Order soon</small></p>
         </div>
     );
 };
